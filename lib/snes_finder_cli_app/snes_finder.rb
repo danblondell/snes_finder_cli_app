bcf85d171bcff8113de07effb836267cb252e9ac
Dan Blondell <danblondell@gmail.com>
2017-08-27 17:34:50 -0400

    Creates classes for SNES finders, with each store its own inheriting class

---
+++ lib/snes_finder_cli_app/snes_finder.rb
@@ -0,0 +1,21 @@
+class SnesFinderCliApp::Finder
+  attr_accessor :url
+
+  def initialize
+    @url == @@url
+  end
+
+end
+
+
+class BestBuy < Finder
+  @@url = "https://www.bestbuy.com/site/nintendo-entertainment-system-snes-classic-edition/5919830.p?skuId=5919830"
+end
+
+class Amazon < Finder
+  @@url = "https://www.amazon.com/Super-NES-Classic/dp/B0721GGGS9/ref=sr_1_1?ie=UTF8&qid=1503868738&sr=8-1&keywords=snes+classic"
+end
+
+class Target < Finder
+  @@url = "https://www.target.com/p/nintendo-174-super-nes-classic-edition/-/A-52826093#lnk=sametab"
+end
