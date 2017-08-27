92e0e78e592c6c35c91003bde07ef3a0f1d0892f
Dan Blondell <danblondell@gmail.com>
2017-08-27 17:30:06 -0400

    Added dummy functionality to be filled with class methods

--- lib/snes_finder_cli_app/cli.rb
+++ lib/snes_finder_cli_app/cli.rb
@@ -1,6 +1,25 @@
 class SnesFinderCliApp::CLI
 
   def call
-    puts "You have successfully started the program."
+    puts "Here are the current listings for SNES Classic"
+    puts "1. Best Buy - out of stock"
+    puts "2. Amazon - out of stock"
+    puts "3. Target - PRE-ORDER!"
+    puts "\n"
+    puts "Press ENTER to refresh or type the number of the link you are interested in, or type exit to quit."
+
+    input = gets.strip
+
+    if input.downcase == "exit"
+      puts "Come again soon!"
+    elsif input.to_i == 1
+      puts "Best Buy URL"
+    elsif input.to_i == 1
+      puts "Amazon URL"
+    elsif input.to_i == 2
+      puts "Target URL"
+    else
+      call
+    end
   end
 end
